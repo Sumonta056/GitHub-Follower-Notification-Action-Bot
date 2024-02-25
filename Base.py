9313fb77587a62418de029c451910e467b93aa73
Sumonta Saha Mridul <61287791+Sumonta056@users.noreply.github.com>
2024-02-25 22:54:42 +0600

    fix : Workflow YAML and Directory

--- Base.py
+++
@@ -1,9 +0,0 @@
-# server = smtplib.SMTP("smtp.gmail.com", 587)
-#
-# server.starttls()
-#
-# server.login("gitfollowernotifierbysumonta56@gmail.com", "ibiiwjaehbdupqnt")
-#
-# server.sendmail('gitfollowernotifierbysumonta56@gmail.com','promimojumder8@gmail.com','Send Me PUTU')
-#
-# print("ok")
