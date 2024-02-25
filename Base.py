6770700b34e6beed15f89967bc1e6b3dfdabf067
Sumonta Saha Mridul <61287791+Sumonta056@users.noreply.github.com>
2024-02-25 22:09:17 +0600

    add:Base Code for emailing Users

---
+++ Base.py
@@ -0,0 +1,9 @@
+# server = smtplib.SMTP("smtp.gmail.com", 587)
+#
+# server.starttls()
+#
+# server.login("gitfollowernotifierbysumonta56@gmail.com", "ibiiwjaehbdupqnt")
+#
+# server.sendmail('gitfollowernotifierbysumonta56@gmail.com','promimojumder8@gmail.com','Send Me PUTU')
+#
+# print("ok")
